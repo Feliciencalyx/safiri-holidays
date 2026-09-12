fe85f58de994b8acc0d0cf409f587886b0d13b21
Feliciencalyx <feliciencalylx@gmail.com>
2026-09-12 12:51:08 +0200

    ci: add missing gradle wrapper scripts and resolve NDK version for GitHub Actions

--- flutter_application_v2/android/app/build.gradle.kts
+++ flutter_application_v2/android/app/build.gradle.kts
@@ -7,7 +7,7 @@ plugins {
 android {
     namespace = "com.example.flutter_application_v2"
     compileSdk = flutter.compileSdkVersion
-    ndkVersion = "28.2.13676358"
+    ndkVersion = flutter.ndkVersion
 
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_17
